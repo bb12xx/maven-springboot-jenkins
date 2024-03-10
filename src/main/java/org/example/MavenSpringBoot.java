33aea7a5e1d3d9a8dcd051fb7a33b441057e12a0
Bristirekha Bhuyan <Bristibhuyan@gmail.com>
2024-03-10 10:10:50 +0530

    First commit

--- src/main/java/org/example/MavenSpringBoot.java
+++ src/main/java/org/example/MavenSpringBoot.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 @SpringBootApplication
+//developed by Bristi
 public class MavenSpringBoot {
     public static void main(String[] args) {
         SpringApplication.run(MavenSpringBoot.class,args);
